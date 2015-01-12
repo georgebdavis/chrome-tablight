a7450de2f570c7eeb4d93105207c500c323e1a7e
George Davis <george.b.davis@gmail.com>
2015-01-11 17:57:32 -1000

    Removed Google Analytics tracking.
    Found information about what was being done here: https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApi_gaq

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -2,16 +2,6 @@
 
 console.log('Tab Search starting!');
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-58195300-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 function handleSpotlight(request) {
   var port = this;
 
@@ -20,7 +10,6 @@ function handleSpotlight(request) {
       chrome.tabs.sendMessage(port.sender.tab.id, {tabs: tabs});
     });
   } else if (request.hasOwnProperty('activate')) {
-    _gaq.push(['_trackEvent', 'Background', 'Activate']);
     chrome.tabs.update(request.activate, {active: true});
   }
 }
